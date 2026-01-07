26febc0a31554996a0bfe5117f76515f57909247
JaromiZsombi <jaromi.zsombi@gmail.com>
2026-01-07 13:27:40 +0100

    uj dolog hozzaadasa

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -3,11 +3,7 @@ import React from 'react'
 const Add = () => {
   return (
     <div>
-      <h2>
-        sdawdawdawdadd as dad asd ad s
-      </h2>
-      <h1>Add sd asd ad ad asd asd asd task</h1>
-      <h2>asd ad asdasd as </h2>
+      <h6>Ez az új cucc haló a többit idk</h6>
     </div>
   )
 }
