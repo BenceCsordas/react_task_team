65f5037cf268c470bb473327a9b42009e96b6c37
JaromiZsombi <jaromi.zsombi@gmail.com>
2026-01-08 11:33:50 +0100

    AddTask megjavítva

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Add } from './components/AddTask'
+import { AddTask } from './components/AddTask'
 
 function App() {
  
   return (
     <div>
-      <Add/>
+      <AddTask/>
     </div>
   )
 }
